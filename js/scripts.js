9973f08dc7a878e58554e2bbcf89313ae986bcb5
natagur <gur.natalia.n@yandex.ru>
2023-07-07 18:22:55 +0400

    компоненты

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 // выбор дня
 var selectElement = document.getElementById("birthday");
 
-for (var i = 1; i <= 31; i++) {
-  var option = document.createElement("option");
-  option.value = i;
-  option.text = i;
-  selectElement.appendChild(option);
+if (selectElement) {
+  for (var i = 1; i <= 31; i++) {
+    var option = document.createElement("option");
+    option.value = i;
+    option.text = i;
+    selectElement.appendChild(option);
+  }
 }
 
 // выбор месяца
@@ -13,21 +15,73 @@ var selectElement = document.getElementById("month");
 
 var months = ["январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"];
 
-for (var i = 0; i < months.length; i++) {
-  var option = document.createElement("option");
-  option.value = months[i];
-  option.text = months[i];
-  selectElement.appendChild(option);
+if (selectElement) {
+  for (var i = 0; i < months.length; i++) {
+    var option = document.createElement("option");
+    option.value = months[i];
+    option.text = months[i];
+    selectElement.appendChild(option);
+  }
 }
 
 // выбор года
-// Получаем ссылку на элемент <select>
 var selectElement = document.getElementById("year");
 
-// Добавляем <option> с годами от 1970 до 2023
-for (var year = 1970; year <= 2023; year++) {
-  var option = document.createElement("option");
-  option.value = year;
-  option.text = year;
-  selectElement.appendChild(option);
+if (selectElement) {
+  for (var year = 1970; year <= 2023; year++) {
+    var option = document.createElement("option");
+    option.value = year;
+    option.text = year;
+    selectElement.appendChild(option);
+  }
 }
+
+
+/* html star */
+function star(){
+  var starblock = document.getElementById("star__block");
+
+  var count = starblock.getAttribute("data-count");
+
+  var elements = starblock.getElementsByClassName("star__element");
+
+  for (var i = 0; i < 5; i++) {
+    elements[i].classList.remove("active");
+  }
+
+  for (var i = 0; i < count; i++) {
+    elements[i].classList.add("active");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  var starblock = document.getElementById("star__block");
+
+  if (starblock) {
+    star();
+  let search = document.querySelectorAll('.star__element');
+  let clickSearch = function(event){
+
+    var index = Array.prototype.indexOf.call(starblock.children, this);
+
+    starblock.setAttribute("data-count", index + 1);
+    star();
+
+    event.preventDefault();
+  };
+
+  search.forEach(function(el){
+
+    el.addEventListener('click' ,clickSearch);
+
+  });
+  }
+
+});
+
+function updateProgressBar(progress) {
+  var progressBar = document.getElementById('myProgressBar');
+  progressBar.style.width = progress + '%';
+}
+
+updateProgressBar(20);
